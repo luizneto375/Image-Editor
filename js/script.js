37f1bd2c912f92e2a25380df814ca146f5419ae5
luizneto375 <114784434+luizneto375@users.noreply.github.com>
2023-06-08 09:46:23 -0300

    ativando as funçções da pagina

--- js/script.js
+++ js/script.js
@@ -9,3 +9,17 @@ resetFilterBtn = document.querySelector(".reset-filter"),
 chooseImgBtn = document.querySelector(".choose-img"),
 saveImgBtn = document.querySelector(".save-img");
 
+let brightness = "100", saturation = "100", inversion = "0", grayscale = "0";
+
+let rotate = 0, flipHorizontal = 1, flipVertical = 1;
+
+const loadImage = () => {
+    let file = fileInput.files[0];
+    if(!file) return;
+    previewImg.src = URL.createObjectURL(file);
+    previewImg.addEventListener("load", () => {
+        resetFilterBtn.click();
+        document.querySelector(".container").classList.remove("disable");
+    });
+}
+
