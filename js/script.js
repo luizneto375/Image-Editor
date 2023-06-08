220283ee16aed6d6ad606cf47ff5f6f99bfe65a8
luizneto375 <114784434+luizneto375@users.noreply.github.com>
2023-06-08 01:21:24 -0300

    comecar a fazer a interatividade com o site

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const fileInput = document.querySelector(".file-input"),
+filterOptions = document.querySelectorAll(".filter button"),
+filterName = document.querySelector("filter-info .name"),
+filterValue = document.querySelector(".filter-info .value"),
+filterSlider = document.querySelector(".slider input"),
+rotateOptions = document.querySelectorAll(".rotate button"),
+previewImg = document.querySelector(".preview-img img"),
+resetFilterBtn = document.querySelector(".reset-filter"),
+chooseImgBtn = document.querySelector(".choose-img"),
+saveImgBtn = document.querySelector(".save-img");
+
